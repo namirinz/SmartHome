276e66832a8b4feebe34c8c6232b5d8414cd5360
namirinz <nathmcfc31115@gmail.com>
2020-05-15 12:13:39 +0700

    Update pi_face_recognition.py

--- pi_face_recognition.py
+++ pi_face_recognition.py
@@ -36,8 +36,8 @@ detector = cv2.CascadeClassifier(args["cascade"])
 
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start() #uncomment if you use webcam camera
-#vs = VideoStream(usePiCamera=True).start() #uncomment if you use raspberry pi camera
+#vs = VideoStream(src=0).start() # uncomment if you use webcam camera
+vs = VideoStream(usePiCamera=True).start() # uncomment if you use raspberry pi camera
 time.sleep(2.0)
 
 # start the FPS counter
